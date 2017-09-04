0bdec10dfede4a66ae47bd19815d1defcf580d28
misterhackney <mail@misterhackney.com>
2017-09-03 20:24:41 -0400

    Trimming

--- include/classes/tzn_mysql.php
+++ include/classes/tzn_mysql.php
@@ -158,6 +158,7 @@ class TznDbConnection {
 
 	function queryAffect($qry) {
 		if ($this->isConnected()) {
+		$qry = mysqli_real_escape_string($this->_dbLink, $qry);
 		$qry =	$this->_dbLink->real_escape_string($qry);
 			$this->_dbLink->query($qry);
 			if (($affected_row = $this->_dbLink->affected_rows) == -1) {
