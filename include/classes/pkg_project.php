c963c477e4665b3756a25f888cf411200faa1628
Matt <mail@misterhackney.com>
2018-04-03 22:54:40 -0400

    Large Date

--- include/classes/pkg_project.php
+++ include/classes/pkg_project.php
@@ -1,28 +1,5 @@
 <?php
-/****************************************************************************\
-* TaskFreak!                                                                 *
-* multi user                                                                 *
-******************************************************************************
-* Version: 0.6.3                                                             *
-* Authors: Stan Ozier <taskfreak@gmail.com>                                  *
-* License:  http://www.gnu.org/licenses/gpl.txt (GPL)                        *
-******************************************************************************
-* This file is part of "TaskFreak! multi user" program.                      *
-*                                                                            *
-* TaskFreak! multi user is free software; you can redistribute it and/or     *
-* modify it under the terms of the GNU General Public License as published   *
-* by the Free Software Foundation; either version 2 of the License, or (at   *  
-* your option) any later version.                                            *
-*                                                                            *
-* TaskFreak! multi user is distributed in the hope that it will be           *
-* useful, but WITHOUT ANY WARRANTY; without even the implied warranty of     *
-* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the              *
-* GNU General Public License for more details.                               *
-*                                                                            *
-* You should have received a copy of the GNU General Public License          *
-* along with this program; if not, write to the Free Software                *
-* Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA *
-\****************************************************************************/
+
 
 //define('PRJ_DTE_NOW',strtotime(gmdate('Y-m-d',time()+Tzn::_getUserTZ())));
 define('PRJ_DTE_NOW',strtotime(date('Y-m-d',time()+Tzn::_getUserTZ())));
@@ -238,9 +215,9 @@ class ItemStats extends Item
 					    if (is_array($GLOBALS['langTznDate']) && array_key_exists($day,$GLOBALS['langTznDate'])) {
 					       $day = ucfirst($GLOBALS['langTznDate'][$day]);
 					    }
-					    return '<span class="small">'.ucFirst($day).'</span>';
+					    return '<span>'.ucFirst($day).'</span>';
 					} else {
-					    return '<span class="small">'
+					    return '<span>'
 							.$this->getDte('deadlineDate',($short)?'SHT':'SHX','')
 							.'</span>';
 					}
